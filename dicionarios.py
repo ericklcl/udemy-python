5550a14c0371b3df9cd7a5d87770a2625c4d9243
ericklcl <ericklcl@gmail.com>
2021-03-19 19:34:34 -0300

    Alteracao no arquivo do dicionario e tipo_none.

--- dicionarios.py
+++ dicionarios.py
@@ -31,3 +31,24 @@ print(paises['py'])
 print("\nForma 2 - Acessando via get - Recomendada")
 print(paises.get('br'))
 print(paises.get('ru'))
+
+russia = paises.get('ru')
+
+pais = paises.get('py')
+if pais:
+    print(f'Encontrei o pais {pais}'.format(pais=pais))
+else:
+    print(f'Nao encontrei o pais {pais}')
+
+# Eh possivel definir um valor padrao para o dicionario usando o get, caso nao encontremos o objeto com a chave
+# informada
+
+pais = paises.get('ru', 'Nao encontrado')
+print(f'Encontrei o pais {pais}'.format(pais=pais))
+
+pais = paises.get('py', 'Nao encontrado')
+print(f'Encontrei o pais {pais}'.format(pais=pais))
+
+# Caso o get nao encontre o objeto com a chave informada, será retornado o valor None e não será gerado KeyError
+
+
--- tipo_none.py
+++ tipo_none.py
@@ -5,9 +5,23 @@ O tipo de dado None em Python representa o tipo sem tipo, ou poderia ser conheci
 como tipo vazio, porém, falar que é um tipo sem tipo é mais apropriado.
 
 O tipo None é SEMPRE especificado com a primeira letra maiuscula
+
+Quando utilizamos ?
+
+Podemos utilizar None quando queremos criar uma variavel inacializa-la com um tipo sem tipo,
+antes de recebermos um valor final.
+
+Obs: O tipo None em python é sempre considerado como False.
 """
 
 numeros = None
 
 print(numeros)
 print(type(numeros))
+
+numeros = 1.44, 1.34, 5.67
+
+print(numeros)
+print(type(numeros))
+
+
