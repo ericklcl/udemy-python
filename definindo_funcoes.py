8b1ee655e6ba5d9ecd0fd82238588cd2943064c0
ericklcl <ericklcl@gmail.com>
2021-04-14 07:36:12 -0300

    adicionado o arquivo definindo_funcoes.py

---
+++ definindo_funcoes.py
@@ -0,0 +1,10 @@
+cores =  ['verde', 'amarelo', 'azul', 'branco']
+
+# Utilizando a funcao integrada (built-in) do python 
+print(cores)
+
+curso = 'Programacao em Python Essencial'
+print(curso)
+
+cores.append('roxo')
+print(cores)
